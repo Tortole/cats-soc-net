81ed4b4b0c761b03ea0c850d794ba46c36c9146b
Tortole <xorid@mail.ru>
2026-02-18 20:11:38 +0800

    feat: Add basic backend
    - installed and configured MongoDB
    - configured launch.json for debugging a project in VS Code
    - added blog module
      - schema, interface and DTO for model field in DB
      - service for getting, adding, editing and deleting posts
      - controller for API for getting, adding, editing and deleting posts
      - ValidateObjectId for validating the presence of posts
    - disabled some eslint errors for basic feature development

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (config: ConfigService) => ({
+                uri: config.get<string>('MONGODB_URI'),
+            }),
+        }),
+        BlogModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
